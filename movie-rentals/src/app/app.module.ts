12e717ec8d7a79540f75f5e671d2e85552554886
johnn93 <112646294+johnn93@users.noreply.github.com>
2023-05-04 11:38:59 +0300

    server and clients list

--- movie-rentals/src/app/app.module.ts
+++ movie-rentals/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ClientsListComponent } from './clients/clients-list/clients-list.component';
+import {InMemoryDataService} from "./shared/in-memory-data.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsComponent,
+    ClientsListComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
     AppRoutingModule
   ],
   providers: [],
