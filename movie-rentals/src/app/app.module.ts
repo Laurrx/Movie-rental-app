cc274d1e667b6bfd7a7c7abae706a0dfce0e1393
johnn93 <112646294+johnn93@users.noreply.github.com>
2023-05-22 08:31:35 +0300

    am facut formul pentru rent movie

--- movie-rentals/src/app/app.module.ts
+++ movie-rentals/src/app/app.module.ts
@@ -1,23 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from "@angular/common/http";
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ClientsComponent } from './clients/clients.component';
-import { ClientsListComponent } from './clients/clients-list/clients-list.component';
-import { ClientNewComponent } from './clients/client-new/client-new.component';
-import { ClientEditComponent } from './clients/client-edit/client-edit.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ClientsComponent} from './clients/clients.component';
+import {ClientsListComponent} from './clients/clients-list/clients-list.component';
+import {ClientNewComponent} from './clients/client-new/client-new.component';
+import {ClientEditComponent} from './clients/client-edit/client-edit.component';
 import {FormsModule} from "@angular/forms";
-import { MoviesComponent } from './movies/movies.component';
-import { MoviesListComponent } from './movies/movies-list/movies-list.component';
-import { MovieDetailComponent } from './movies/movie-detail/movie-detail.component';
-import { MovieNewComponent } from './movies/movie-new/movie-new.component';
+import {MoviesComponent} from './movies/movies.component';
+import {MoviesListComponent} from './movies/movies-list/movies-list.component';
+import {MovieDetailComponent} from './movies/movie-detail/movie-detail.component';
+import {MovieNewComponent} from './movies/movie-new/movie-new.component';
 import {Ng2SearchPipeModule} from "ng2-search-filter";
 import {MyCustomSearchPipe} from "./movies/shared/my-custom-search.pipe";
-import { RentalComponent } from './rental/rental.component';
-import { RentalListComponent } from './rental/rental-list/rental-list.component';
-import { ApiService } from './shared/api.service';
-import { RentMovieComponent } from './rental/rent-movie/rent-movie.component';
+import {RentalComponent} from './rental/rental.component';
+import {RentalListComponent} from './rental/rental-list/rental-list.component';
+import {ApiService} from './shared/api.service';
+import {RentMovieComponent} from './rental/rent-movie/rent-movie.component';
 
 
 @NgModule({
@@ -34,16 +34,17 @@ import { RentMovieComponent } from './rental/rent-movie/rent-movie.component';
     MyCustomSearchPipe,
     RentalComponent,
     RentalListComponent,
-    RentMovieComponent
+    RentMovieComponent,
   ],
   imports: [
     BrowserModule,
     Ng2SearchPipeModule,
     HttpClientModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
