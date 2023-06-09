cb0b9aafc01e0c3454cc39eb93060c07aac420b9
johnn93 <112646294+johnn93@users.noreply.github.com>
2023-06-09 23:16:35 +0300

    filter in progress

--- movie-rentals/src/app/movies/movies-list/movies-list.component.ts
+++ movie-rentals/src/app/movies/movies-list/movies-list.component.ts
@@ -14,7 +14,7 @@ import {RentMovieComponent} from "../../rental/rent-movie/rent-movie.component";
 export class MoviesListComponent implements OnInit {
   movies: Array<Movie> = [];
   searchTerm = '';
-  filteredMovies=this.movies;
+  filteredMovies = this.movies;
   debouncedSearchTerm = '';
   modelChanged = new Subject<string>();
   moviesSearchCriterias = ['title', 'description'];
@@ -23,10 +23,11 @@ export class MoviesListComponent implements OnInit {
   selectedMovie?: any;
   isSelectedMovie = false;
   filterType = 'genre,releaseYear'
+  selectYear=0;
   filter = '';
   value = [
     {
-      value: 'ScienceFiction',
+      value: 'Science Fiction',
       display: 'Science Fiction'
     },
     {
@@ -60,6 +61,7 @@ export class MoviesListComponent implements OnInit {
     this.movieServices.getMovies()
       .subscribe(movies => {
         this.movies = movies
+        this.filteredMovies = this.movies;
         this.isLoading = false;
       });
 
@@ -105,8 +107,8 @@ export class MoviesListComponent implements OnInit {
     this.filter = (filter === 'default') ? '' : filter;
   }
 
-  empyArray(array:any){
-    array.splice(0,1);
+  empyArray(array: any) {
+    array.splice(0, 1);
     return array;
   }
 
@@ -116,29 +118,52 @@ export class MoviesListComponent implements OnInit {
       this.searchFilterCriterias.forEach((element: any, index: any) => {
         if (element === filter) this.searchFilterCriterias.splice(index, 1)
       })
-      console.log(this.searchFilterCriterias)
-      if(this.searchFilterCriterias.length===0){
-        this.filteredMovies=this.movies
-      }
-
-    } else {
-      if(this.searchFilterCriterias.length===1 && this.searchFilterCriterias[0]==='default'){
-        console.log(this.searchFilterCriterias[0])
-        // this.searchFilterCriterias=this.searchFilterCriterias.splice(0,1);
-        this.empyArray(this.searchFilterCriterias);
-      }
-      this.searchFilterCriterias = [...this.searchFilterCriterias, filter];
-      console.log(this.searchFilterCriterias)
       this.filteredMovies=this.movies.filter(movie => {
         let found = false;
         this.searchFilterCriterias.forEach((filter:any)=>{
-          if (movie.genre.toLowerCase() === filter) {
-          found = true
-        }
+          if (movie.genre.toLowerCase() === filter.toLowerCase() || movie.releaseYear == this.selectYear) {
+            console.log(filter.value)
+            found = true
+          }
         })
         return found;
+      })
+      console.log(this.searchFilterCriterias)
+      if (this.searchFilterCriterias.length === 0) {
+        this.filteredMovies = this.movies
+      }
+
+    } else {
+      if (this.searchFilterCriterias.length === 1 && this.searchFilterCriterias[0] === 'default') {
+        console.log(this.searchFilterCriterias[0])
+        this.searchFilterCriterias = this.searchFilterCriterias.splice(0, 1);
+        // this.empyArray(this.searchFilterCriterias);
+      }
+      this.searchFilterCriterias = [...this.searchFilterCriterias, filter];
+      console.log(this.searchFilterCriterias)
+      this.filteredMovies = this.movies.filter(movie => {
+        let found = false;
+        this.searchFilterCriterias.forEach((filter: any) => {
+          if (movie.genre.toLowerCase() === filter.toLowerCase()) {
+            found = true
+          }
         })
+        return found;
+      })
 
     }
+  }
+
+  selectedYear(value: string) {
+    this.selectYear=+value;
+    console.log(value)
+    this.filteredMovies = this.movies.filter(movie => {
+      let found = false;
+        if (movie.releaseYear == +value) {
+          found = true
+        }
+
+      return found;
+    })
   }
 }
