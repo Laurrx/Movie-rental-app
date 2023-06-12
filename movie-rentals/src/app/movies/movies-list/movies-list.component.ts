7620a9615c4ce2fb7475130d66f00aae2293b0da
johnn93 <112646294+johnn93@users.noreply.github.com>
2023-06-12 12:45:40 +0300

    filter in progress

--- movie-rentals/src/app/movies/movies-list/movies-list.component.ts
+++ movie-rentals/src/app/movies/movies-list/movies-list.component.ts
@@ -103,67 +103,58 @@ export class MoviesListComponent implements OnInit {
     this.isSelectedMovie = $event;
   }
 
-  onSelectedFilter(filter: string) {
-    this.filter = (filter === 'default') ? '' : filter;
-  }
-
-  empyArray(array: any) {
-    array.splice(0, 1);
-    return array;
-  }
-
+  // onSelectedFilter(filter: string) {
+  //   this.filter = (filter === 'default') ? '' : filter;
+  // }
 
   checkBoxSelected(filter: string) {
     if (this.searchFilterCriterias.includes(filter)) {
       this.searchFilterCriterias.forEach((element: any, index: any) => {
         if (element === filter) this.searchFilterCriterias.splice(index, 1)
       })
-      this.filteredMovies=this.movies.filter(movie => {
-        let found = false;
-        this.searchFilterCriterias.forEach((filter:any)=>{
-          if (movie.genre.toLowerCase() === filter.toLowerCase() || movie.releaseYear == this.selectYear) {
-            console.log(filter.value)
-            found = true
-          }
-        })
-        return found;
-      })
-      console.log(this.searchFilterCriterias)
-      if (this.searchFilterCriterias.length === 0) {
-        this.filteredMovies = this.movies
-      }
+      // this.filteredMovies=this.movies.filter(movie => {
+      //   let found = false;
+      //   this.searchFilterCriterias.forEach((filter:any)=>{
+      //     if (movie.genre.toLowerCase() === filter.toLowerCase() || movie.releaseYear == this.selectYear) {
+      //       console.log("the filter value is"+filter.value)
+      //       found = true
+      //     }
+      //   })
+      //   return found;
+      // })
+      console.log("the searchFilterCriterias are "+this.searchFilterCriterias + "and length is "+this.searchFilterCriterias.length
+                  +"and the filteredMovies are "+this.filteredMovies)
+
 
     } else {
-      if (this.searchFilterCriterias.length === 1 && this.searchFilterCriterias[0] === 'default') {
-        console.log(this.searchFilterCriterias[0])
-        this.searchFilterCriterias = this.searchFilterCriterias.splice(0, 1);
-        // this.empyArray(this.searchFilterCriterias);
-      }
+      // if (this.searchFilterCriterias.length === 1 && this.searchFilterCriterias[0] === 'default') {
+      //   console.log(this.searchFilterCriterias[0])
+      //   this.searchFilterCriterias = this.searchFilterCriterias.splice(0, 1);
+      // }
       this.searchFilterCriterias = [...this.searchFilterCriterias, filter];
-      console.log(this.searchFilterCriterias)
+    }
+    if (this.searchFilterCriterias.length === 0) {
+      this.filteredMovies=this.movies;
+      console.log("filtered movies are "+this.filteredMovies+"and movies are "+this.movies)
+    }
+    if(this.searchFilterCriterias!=0){
       this.filteredMovies = this.movies.filter(movie => {
         let found = false;
         this.searchFilterCriterias.forEach((filter: any) => {
-          if (movie.genre.toLowerCase() === filter.toLowerCase()) {
+          if (movie.genre.toLowerCase() === filter.toLowerCase() || movie.releaseYear===this.selectYear) {
             found = true
           }
         })
         return found;
       })
-
     }
+
+
+
   }
 
   selectedYear(value: string) {
     this.selectYear=+value;
     console.log(value)
-    this.filteredMovies = this.movies.filter(movie => {
-      let found = false;
-        if (movie.releaseYear == +value) {
-          found = true
-        }
-
-      return found;
-    })
   }
 }
